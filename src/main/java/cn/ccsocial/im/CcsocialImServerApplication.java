0cfa80ce3c7500f277e7bd3cd19a2d838345786e
tonywang <710093245@qq.com>
2022-11-21 18:41:23 +0800

    first commit

---
+++ src/main/java/cn/ccsocial/im/CcsocialImServerApplication.java
@@ -0,0 +1,18 @@
+package cn.ccsocial.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * CCsocialIM启动类
+ *
+ * @author tonywang
+ * @date 2022/11/20
+ */
+@SpringBootApplication
+public class CcsocialImServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcsocialImServerApplication.class, args);
+    }
+
+}
